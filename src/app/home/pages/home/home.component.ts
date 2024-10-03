eb3f416bb8ec56d67163969bfe37783e7d4ce978
edwinlopezz17 <u20201b051@upc.edu.pe>
2024-10-02 21:58:58 -0500

    i18n completed

--- src/app/home/pages/home/home.component.ts
+++ src/app/home/pages/home/home.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ToolbarComponent} from "../../../share/toolbar/toolbar.component";
+import {TranslateModule, } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    ToolbarComponent
+    ToolbarComponent,
+    TranslateModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
 
 }
--- src/app/share/toolbar/toolbar.component.ts
+++ src/app/share/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-toolbar',
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit{
 
+  languages = ['en', 'es']
+
+  constructor(private _translateService: TranslateService) {
+
+  }
+
+  ngOnInit(): void {
+    const defaultLange = 'es'
+    this._translateService.setDefaultLang(defaultLange);
+    this._translateService.use(defaultLange);
+  }
+
+  changeLanguage(language: string):void {
+    this._translateService.setDefaultLang(language);
+    this._translateService.use(language);
+    localStorage.setItem('language',language);
+    alert(language);
+  }
 }
